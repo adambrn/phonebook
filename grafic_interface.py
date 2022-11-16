17a81894130f3faad2dc19df18b82f0536a43fdf
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-11-16 22:58:38 +0700

    add del and edit

--- grafic_interface.py
+++ grafic_interface.py
@@ -1,5 +1,107 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import showerror
+
+# определяем данные для отображения
+from os.path import exists
+import file_operations
+path = 'phonebook.csv'
+data = {}
+if not exists(path):
+    file_operations.creating_csv(path)
+else:
+    data = file_operations.reading_csv(path)
+people = list(tuple(x.values()) for x in data)
+
+#Функция удаления записи
+def delete():
+
+    if tree.selection():
+        selected_item = tree.selection()[0] ## get selected item
+        values = tree.item(selected_item, option="values")
+        people.remove(values)
+        print(people)
+        tree.delete(selected_item)
+    else:
+        showerror(title="Ошибка", message="Выберите запись для удаления!")
+
+def edit():
+    
+    entryIndex = tree.focus()
+    if '' == entryIndex: return
+
+    # Всплывающее окно
+    win = Toplevel()
+    win.title("Редактировать")
+    win.attributes("-toolwindow", True)
+
+    for child in tree.get_children():
+        if child == entryIndex:
+            values = tree.item(child)["values"]
+            break
+
+    last_name_lable = Label(win, text = "Фамилия: ")
+    last_name_entry = Entry(win)
+    last_name_entry.insert(0, values[0]) 
+    last_name_lable.grid(row = 0, column = 0)
+    last_name_entry.grid(row = 0, column = 1)
+
+    name_lable = Label(win, text = "Имя: ")
+    name_entry = Entry(win)
+    name_entry.insert(0, values[1]) 
+    name_lable.grid(row = 1, column = 0)
+    name_entry.grid(row = 1, column = 1)
+
+    father_name_lable = Label(win, text = "Отчество: ")
+    father_name_entry = Entry(win)
+    father_name_entry.insert(0, values[2]) 
+    father_name_lable.grid(row = 2, column = 0)
+    father_name_entry.grid(row = 2, column = 1)
+
+    phone_lable = Label(win, text = "телефон: ")
+    phone_entry = Entry(win)
+    phone_entry.insert(0, values[3]) 
+    phone_lable.grid(row = 3, column = 0)
+    phone_entry.grid(row = 3, column = 1)
+
+    comment_lable = Label(win, text = "Комментарий: ")
+    comment_entry = Entry(win)
+    comment_entry.insert(0, values[4]) 
+    comment_lable.grid(row = 4, column = 0)
+    comment_entry.grid(row = 4, column = 1)
+    
+    def DeleteCurrentEntry(tree):
+        curr = tree.focus()
+
+        if '' == curr: return
+
+        tree.delete(curr)
+    
+    def ConfirmEntry(tree, entry1, entry2, entry3,entry4,entry5):
+        currInd = tree.index(tree.focus())
+        DeleteCurrentEntry(tree)
+        tree.insert('', currInd, values = (entry1, entry2, entry3,entry4,entry5))
+        return True
+
+    def UpdateThenDestroy():
+        if ConfirmEntry(tree, 
+        last_name_entry.get(), 
+        name_entry.get(), 
+        father_name_entry.get(), 
+        phone_entry.get(),
+        comment_entry.get()
+        ):
+            win.destroy()
+
+    okButt = ttk.Button(win, text = "Сохранить")
+    okButt.bind("<Button-1>", lambda e: UpdateThenDestroy())
+    okButt.grid(row = 5, column = 0)
+
+    canButt = ttk.Button(win, text = "Отмена")
+    canButt.bind("<Button-1>", lambda c: win.destroy())
+    canButt.grid(row = 5, column = 1)
+
+
 root = Tk()     # создаем корневой объект - окно
 root.title('Телефонный справочник')     # устанавливаем заголовок окна
 root.geometry('1000x500')    # устанавливаем размеры окна
@@ -23,9 +125,7 @@ Label(top_frame, text = 'Поиск').grid(row = 1, column = 1, sticky = N)
 ttk.Entry(top_frame, width = 40).grid(row = 1, column = 2)
 ttk.Button(top_frame, text = 'Найти',).grid(row = 1, column = 3)
 #таблица
-# определяем данные для отображения
-people = [('Иван','Иванович','Иванов','222-333','первый'),('Иван','Иванович','Иванов','222-555','второй')]
- 
+
 # определяем столбцы
 columns = ('name','father_name','last_name', 'phone', 'comment')
  
@@ -48,6 +148,6 @@ bottom_frame = Frame(root)
 bottom_frame.pack()
 
 ttk.Button(bottom_frame, text = 'Добавить',).grid(row = 1, column = 1)
-ttk.Button(bottom_frame, text = 'Редактировать',).grid(row = 1, column = 2)
-ttk.Button(bottom_frame, text = 'Удалить',).grid(row = 1, column = 3)
+ttk.Button(bottom_frame, text = 'Редактировать',command=edit).grid(row = 1, column = 2)
+ttk.Button(bottom_frame, text = 'Удалить',command=delete).grid(row = 1, column = 3)
 root.mainloop()
--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if not exists(path):
     file_operations.creating_csv(path)
 else:
     data = file_operations.reading_csv(path)
-
-console_interface.view(data)
+people = list(tuple(x.values()) for x in data)
+print(people)
+#console_interface.view(data)
