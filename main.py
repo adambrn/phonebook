28c878cf4705a50ff90fe6f5fcdf4a6350f44b4d
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-11-30 20:34:29 +0700

    add bot

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from os.path import exists
 import file_operations
 import db_sqlite
 from grafic_interface import gui_view
-# path = file_operations.get_data_path()
 
-# data = []
-# if not exists(path):
-#     file_operations.creating_csv(path)
-# else:
-#     data = file_operations.reading_csv(path)
+#Запуск бота
+
+
 file_path = file_operations.get_db_path()
 db_sqlite.init_db(file_path)
 data = db_sqlite.db_get(file_path,'people')
